8b8c3b37ab66a30ede77427b5a2d22e27093eb31
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-04-27 16:18:48 -0500

    Readme edits

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -6,8 +6,8 @@ function Jumbotron() {
         <div>
             <div className="jumbotron jumbotron-fluid" id="jumbotron">
                 <div className="container">
-                    <h1 className="display-4"><i className="fas fa-user-friends"></i> <b>Company Directory</b></h1>
-                    <p className="lead">Search by first or last name or click  column heading to sort.</p>
+                    <h1 className="display-4"><b>Employee Directory</b></h1>
+                    <p className="lead">Search by first or last name or click column headings to sort.</p>
                 </div>
             </div>
         </div>
